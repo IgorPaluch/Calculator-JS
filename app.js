09711d689c4f19586583472361cebcec840aa305
Igor Paluch <hello.igor@wp.pl>
2023-02-16 11:13:36 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+     let screen = document.querySelector(".screen");
+     let buttons = document.querySelectorAll(".btn");
+     let clear = document.querySelector(".btn-clear");
+     let equal = document.querySelector(".btn-equal");
+
+     buttons.forEach(function (button) {
+       button.addEventListener("click", function (e) {
+         let value = e.target.dataset.num;
+         screen.value += value;
+       });
+     });
+
+    equal.addEventListener("click", function (e) {
+        if (screen.value === "") {
+             screen.value = ""
+        } else {
+            let answear = eval(screen.value)
+            
+            screen.value = answear;
+         }
+    })
+     
+    clear.addEventListener("click", function (e) {
+        screen.value = "";
+    })
+
